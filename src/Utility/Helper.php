5d149e1b12de9b606fbb64513e50e8745d7b1598
Oliver Steiner <oliver@mollo.ch>
2019-01-09 22:24:09 +0100

    add exceptions to getFieldValue()

--- src/Utility/Helper.php
+++ src/Utility/Helper.php
@@ -6,7 +6,9 @@ use Drupal\Component\Utility\Crypt;
 use Drupal\Core\Entity\EntityStorageException;
 use Drupal\node\Entity\Node;
 use Drupal\node\NodeInterface;
+use Drupal\small_messages\Exceptions\SmmgHelperException;
 use Drupal\taxonomy\Entity\Term;
+use Exception;
 
 class Helper
 {
@@ -75,63 +77,84 @@ class Helper
     }
 
     /**
-     * @param Node $node
+     * @param NodeInterface | Node $node
      * @param string $field_name
      * @param null $term_list
      * @param bool $force_array
      * @return boolean | string | array
+     * @throws Exception
      */
-    public static function getFieldValue(NodeInterface $node, $field_name, $term_list = null, $force_array = false)
+    public static function getFieldValue( $node, $field_name, $term_list = null, $force_array = false)
     {
-        $field_name = 'field_' . $field_name;
         $result = false;
-        if ($node->get($field_name)) {
-            $value = $node->get($field_name)->getValue();
 
-            // single
-            if (count($value) == 1) {
 
-                // Standart Field
-                if ($value && $value[0] && isset($value[0]['value'])) {
-                    $result = $value[0]['value'];
-                }
 
-                // Target Field
-                if ($value && $value[0] && isset($value[0]['target_id'])) {
-                    $result = $value[0]['target_id'];
-                }
+        try {
 
-                // Value is Taxonomy Term
-                if ($term_list) {
-                    $result = $term_list[$result];
-                }
+            // check for 'field_field_NAME'
+            $pos = strpos($field_name, 'field_');
 
-                if ($force_array) {
-                    $arr[] = $result;
-                    $result = $arr;
-                }
+            if ($pos === 0) {
+
+                throw new Exception('Use $field_name without "field_" in HELPER:getFieldValue(' . $field_name.')');
             }
 
-            $i = 0;
-            if (count($value) > 1) {
+            $field_name = 'field_' . $field_name;
 
-                foreach ($value as $item) {
+            if ($node->get($field_name)) {
+                $value = $node->get($field_name)->getValue();
+
+                // single
+                if (count($value) == 1) {
 
                     // Standart Field
-                    if (isset($item['value'])) {
-                        $result[$i] = $item['value'];
+                    if ($value && $value[0] && isset($value[0]['value'])) {
+                        $result = $value[0]['value'];
                     }
 
                     // Target Field
-                    if (isset($item['target_id'])) {
-                        $result[$i] = $item['target_id'];
+                    if ($value && $value[0] && isset($value[0]['target_id'])) {
+                        $result = $value[0]['target_id'];
                     }
-                    $i++;
+
+                    // Value is Taxonomy Term
+                    if ($term_list) {
+                        $result = $term_list[$result];
+                    }
+
+                    if ($force_array) {
+                        $arr[] = $result;
+                        $result = $arr;
+                    }
                 }
 
+                $i = 0;
+                if (count($value) > 1) {
+
+                    foreach ($value as $item) {
+
+                        // Standart Field
+                        if (isset($item['value'])) {
+                            $result[$i] = $item['value'];
+                        }
+
+                        // Target Field
+                        if (isset($item['target_id'])) {
+                            $result[$i] = $item['target_id'];
+                        }
+                        $i++;
+                    }
+
+                }
             }
         }
+        catch
+            (Exception $e) {
 
+                    throw $e;
+
+            }
 
         return $result;
     }
